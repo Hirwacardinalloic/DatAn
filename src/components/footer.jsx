e8cba3cd047c8d50b48e18a6c548f77f30709430
Hirwa Cardinal Loic <cardinaloichirwa@gmail.com>
2025-08-21 01:50:35 +0300

    modified header and index css

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,14 @@
 import { FiGithub } from "react-icons/fi";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaAngellist } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer =()=>{
     return (
         <footer className="socials" style={{position:"relative"}}>
-                <a href="https://github.com/clin2on3mun" target="_blank" className="social-icons github" rel="noreferrer"><FiGithub /></a>
-                <a href="https://www.linkedin.com/in/munana-clinton/" target="_blank" className="social-icons linkedin" rel="noreferrer"><FaLinkedinIn/></a>
-                <a href="https://wellfound.com/u/munana-clinton" target="_blank" className="social-icons angelist" rel="noreferrer"><FaAngellist /></a>
+                <a href="https://github.com/Hirwacardinalloic" target="_blank" className="social-icons github" rel="noreferrer"><FiGithub /></a>
+                <a href="https://www.linkedin.com/in/cardinal-loic-hirwa-48502b242/" target="_blank" className="social-icons linkedin" rel="noreferrer"><FaLinkedinIn/></a>
+                <a href="https://x.com/loicard" target="_blank" className="social-icons Twitter" rel="noreferrer"><FaXTwitter /></a>
                 <a style={{position:"absolute", top: "20%", left:"70%", fontSize:"1.25em"}} className="back" href="#home">back to top</a>
         </footer>
     )
